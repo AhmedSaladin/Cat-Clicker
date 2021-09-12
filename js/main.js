050e4192f15c5aed114172fcff94f12d91e7255c
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-12 17:17:10 +0200

    Updating cat names

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-const catImg = document.getElementsByClassName("cat")[0];
-const clickedNumber = document.getElementsByClassName("click-number")[0];
-let clicked = 0;
+const catImg1 = document.getElementsByClassName("cat")[0];
+const catImg2 = document.getElementsByClassName("cat")[1];
+const clickedCat1Number = document.getElementsByClassName("click-number")[0];
+const clickedCat2Number = document.getElementsByClassName("click-number")[1];
+let clicked1 = 0;
+let clicked2 = 0;
 
-catImg.addEventListener("click", () => {
-  clicked++;
-  clickedNumber.innerHTML = `You have clicked the cat: ${clicked} times`;
+catImg1.addEventListener("click", () => {
+  clicked1++;
+  clickedCat1Number.innerHTML = `You have clicked Lily: ${clicked1} times`;
+});
+catImg2.addEventListener("click", () => {
+  clicked2++;
+  clickedCat2Number.innerHTML = `You have clicked the Nordy: ${clicked2} times`;
 });
