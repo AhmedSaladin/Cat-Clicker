be6dcfd273ac66d20dba1a15ea7c49fd47ed3b08
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-15 09:49:45 +0200

    Append cat img

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-const cats = ["cat.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpg", "cat5.jpg"];
+const cats = [
+  { name: "Lily", path: "cat.jpg", counter: 0 },
+  { name: "Max", path: "cat2.jpg", counter: 0 },
+  { name: "Loby", path: "cat3.jpg", counter: 0 },
+  { name: "Mandy", path: "cat4.jpg", counter: 0 },
+  { name: "Jack", path: "cat5.jpg", counter: 0 },
+];
 const catList = document.getElementsByClassName("cat-list")[0];
+const catContainer = document.getElementsByClassName("cat-container")[0];
+
 for (let i = 0; i < cats.length; i++) {
   const button = document.createElement("button");
-  button.innerText = cats[i].split(".")[0];
+  button.innerText = cats[i].name;
   button.className = "cat-button";
   button.value = i;
   catList.appendChild(button);
 }
-
+const img = document.createElement("img");
+img.src = `img/${cats[0].path}`;
+img.alt = cats[0].name;
+img.className = "cat-pic";
+catContainer.appendChild(img);
