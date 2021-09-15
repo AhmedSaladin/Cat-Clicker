077949ea7ee023bcf91be90c6fc89538d3956b07
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-15 09:41:39 +0200

    Styling cat list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-const catList = document.getElementsByClassName("cat-list");
+const cats = ["cat.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpg", "cat5.jpg"];
+const catList = document.getElementsByClassName("cat-list")[0];
+for (let i = 0; i < cats.length; i++) {
+  const button = document.createElement("button");
+  button.innerText = cats[i].split(".")[0];
+  button.className = "cat-button";
+  button.value = i;
+  catList.appendChild(button);
+}
+
