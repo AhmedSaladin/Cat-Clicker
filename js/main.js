9ecb8aac65cd6f1a75984bcb96314bf7a0e3c9c1
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-15 09:19:18 +0200

    Restructuer index page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-const catImg1 = document.getElementsByClassName("cat")[0];
-const catImg2 = document.getElementsByClassName("cat")[1];
-const clickedCat1Number = document.getElementsByClassName("click-number")[0];
-const clickedCat2Number = document.getElementsByClassName("click-number")[1];
-let clicked1 = 0;
-let clicked2 = 0;
-
-catImg1.addEventListener("click", () => {
-  clicked1++;
-  clickedCat1Number.innerHTML = `You have clicked Lily: ${clicked1} times`;
-});
-catImg2.addEventListener("click", () => {
-  clicked2++;
-  clickedCat2Number.innerHTML = `You have clicked the Nordy: ${clicked2} times`;
-});
+const catList = document.getElementsByClassName("cat-list");
