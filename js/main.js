01617385d54fa48a9e0fc0cd2bbabb313bb3309a
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-15 10:12:09 +0200

    Update buttons behaviors to change imgs

--- js/main.js
+++ js/main.js
@@ -13,14 +13,24 @@ for (let i = 0; i < cats.length; i++) {
   button.innerText = cats[i].name;
   button.className = "cat-button";
   button.value = i;
+  button.onclick = function () {
+    const catPic = document.getElementsByClassName("cat-pic")[0];
+    catPic.src = `img/${cats[this.value].path}`;
+  };
   catList.appendChild(button);
 }
+
 const img = document.createElement("img");
 const p = document.createElement("p");
 p.textContent = `Please don't click on ${cats[0].name}`;
+p.className = "cat-name";
 p.style = "margin-bottom:10px";
 img.src = `img/${cats[0].path}`;
 img.alt = cats[0].name;
 img.className = "cat-pic";
+img.onclick = () => {
+  cats[0].counter++;
+  p.textContent = `You clicked on ${cats[0].name}: ${cats[0].counter} times`;
+};
 catContainer.appendChild(p);
 catContainer.appendChild(img);
