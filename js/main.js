9df43bf5dceba89313e37505ed87dfe84a2dc48d
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-15 09:55:24 +0200

    Adding p tag contain cat name

--- js/main.js
+++ js/main.js
@@ -16,7 +16,11 @@ for (let i = 0; i < cats.length; i++) {
   catList.appendChild(button);
 }
 const img = document.createElement("img");
+const p = document.createElement("p");
+p.textContent = `Please don't click on ${cats[0].name}`;
+p.style = "margin-bottom:10px";
 img.src = `img/${cats[0].path}`;
 img.alt = cats[0].name;
 img.className = "cat-pic";
+catContainer.appendChild(p);
 catContainer.appendChild(img);
