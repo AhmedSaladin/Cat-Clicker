3399749c08e47a789438c780ae2402a0c4e2f2b6
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-16 17:08:49 +0200

    Sepration Version:1.0

--- js/main.js
+++ js/main.js
@@ -6,26 +6,25 @@ const cats = [
   { name: "Jack", path: "cat5.jpg", counter: 0 },
 ];
 
-const catContainer = document.getElementsByClassName("cat-container")[0];
-const img = document.createElement("img");
-const p = document.createElement("p");
-p.textContent = `Please don't click on ${cats[0].name}`;
-p.className = "cat-name";
-p.style = "margin-bottom:10px";
-img.src = `img/${cats[0].path}`;
-img.alt = cats[0].name;
-img.className = "cat-pic";
-img.onclick = () => {
-  cats[0].counter++;
-  p.textContent = `You clicked on ${cats[0].name}: ${cats[0].counter} times`;
-};
-catContainer.appendChild(p);
-catContainer.appendChild(img);
-
-
- const view={
-   catArea:,
-   catList:(list)=>{
+const view = {
+  catArea: (cat) => {
+    const catContainer = document.getElementsByClassName("cat-container")[0];
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    p.textContent = `Please don't click on ${cat.name}`;
+    p.className = "cat-name";
+    p.style = "margin-bottom:10px";
+    img.src = `img/${cat.path}`;
+    img.alt = cat.name;
+    img.className = "cat-pic";
+    img.onclick = () => {
+      cat.counter++;
+      p.textContent = `You clicked on ${cat.name}: ${cat.counter} times`;
+    };
+    catContainer.appendChild(p);
+    catContainer.appendChild(img);
+  },
+  catList: (list) => {
     const catList = document.getElementsByClassName("cat-list")[0];
     for (let i = 0; i < list.length; i++) {
       const button = document.createElement("button");
@@ -35,15 +34,17 @@ catContainer.appendChild(img);
       button.onclick = function () {
         const catPic = document.getElementsByClassName("cat-pic")[0];
         const catName = document.getElementsByClassName("cat-name")[0];
-    
+
         if (list[this.value].counter != 0) {
           catName.textContent = `You clicked on ${list[this.value].name}: ${
             list[this.value].counter
           } times`;
         } else {
-          catName.textContent = `Please don't click on ${list[this.value].name}`;
+          catName.textContent = `Please don't click on ${
+            list[this.value].name
+          }`;
         }
-    
+
         catPic.src = `img/${list[this.value].path}`;
         catPic.onclick = () => {
           list[this.value].counter++;
@@ -54,6 +55,18 @@ catContainer.appendChild(img);
       };
       catList.appendChild(button);
     }
-   },
-   init:
- }
+  },
+  init: function (list, cat) {
+    this.catArea(cat);
+    this.catList(list);
+  },
+};
+
+const octopus = {
+  model: cats,
+  view: view,
+  init: function () {
+    this.view.init(this.model, this.model[0]);
+  },
+};
+octopus.init();
