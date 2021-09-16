6b58e7dd4fa637ad842abd84bcb1084df3834aad
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-16 18:48:44 +0200

    Sepration Version:2.0

--- js/main.js
+++ js/main.js
@@ -1,72 +1,80 @@
-const cats = [
-  { name: "Lily", path: "cat.jpg", counter: 0 },
-  { name: "Max", path: "cat2.jpg", counter: 0 },
-  { name: "Loby", path: "cat3.jpg", counter: 0 },
-  { name: "Mandy", path: "cat4.jpg", counter: 0 },
-  { name: "Jack", path: "cat5.jpg", counter: 0 },
-];
+const catModel = {
+  currentCat: null,
+  cats: [
+    { name: "Lily", path: "cat.jpg", counter: 0 },
+    { name: "Max", path: "cat2.jpg", counter: 0 },
+    { name: "Loby", path: "cat3.jpg", counter: 0 },
+    { name: "Mandy", path: "cat4.jpg", counter: 0 },
+    { name: "Jack", path: "cat5.jpg", counter: 0 },
+  ],
+};
 
-const view = {
-  catArea: (cat) => {
-    const catContainer = document.getElementsByClassName("cat-container")[0];
-    const img = document.createElement("img");
-    const p = document.createElement("p");
-    p.textContent = `Please don't click on ${cat.name}`;
-    p.className = "cat-name";
-    p.style = "margin-bottom:10px";
-    img.src = `img/${cat.path}`;
-    img.alt = cat.name;
-    img.className = "cat-pic";
-    img.onclick = () => {
-      cat.counter++;
-      p.textContent = `You clicked on ${cat.name}: ${cat.counter} times`;
+const catView = {
+  init: function () {
+    this.catImg = document.getElementsByClassName("cat-pic")[0];
+    this.catName = document.getElementsByClassName("cat-name")[0];
+    this.catImg.onclick = () => {
+      octopus.incrementCatCounter();
     };
-    catContainer.appendChild(p);
-    catContainer.appendChild(img);
+    this.render();
   },
-  catList: (list) => {
-    const catList = document.getElementsByClassName("cat-list")[0];
-    for (let i = 0; i < list.length; i++) {
+
+  render: function () {
+    const currentCat = octopus.getCurrentCat();
+    if (currentCat.counter !== 0)
+      this.catName.textContent = `You clicked on ${currentCat.name}: ${currentCat.counter} times`;
+    else this.catName.textContent = `Please don't click on ${currentCat.name}`;
+    this.catImg.src = `img/${currentCat.path}`;
+    this.catImg.alt = currentCat.name;
+  },
+};
+
+const catListView = {
+  init: function () {
+    this.catList = document.getElementsByClassName("cat-list")[0];
+    this.render();
+  },
+
+  render: function () {
+    const cats = octopus.getCats();
+    const setCurrentCat = octopus.setCurrentCat;
+    for (let i = 0; i < cats.length; i++) {
+      const cat = cats[i];
       const button = document.createElement("button");
-      button.innerText = list[i].name;
+      button.innerText = cat.name;
       button.className = "cat-button";
-      button.value = i;
       button.onclick = function () {
-        const catPic = document.getElementsByClassName("cat-pic")[0];
-        const catName = document.getElementsByClassName("cat-name")[0];
-
-        if (list[this.value].counter != 0) {
-          catName.textContent = `You clicked on ${list[this.value].name}: ${
-            list[this.value].counter
-          } times`;
-        } else {
-          catName.textContent = `Please don't click on ${
-            list[this.value].name
-          }`;
-        }
-
-        catPic.src = `img/${list[this.value].path}`;
-        catPic.onclick = () => {
-          list[this.value].counter++;
-          catName.textContent = `You clicked on ${list[this.value].name}: ${
-            list[this.value].counter
-          } times`;
-        };
+        setCurrentCat(cat);
+        catView.render();
       };
-      catList.appendChild(button);
+      this.catList.appendChild(button);
     }
-  },
-  init: function (list, cat) {
-    this.catArea(cat);
-    this.catList(list);
   },
 };
 
 const octopus = {
-  model: cats,
-  view: view,
   init: function () {
-    this.view.init(this.model, this.model[0]);
+    catModel.currentCat = catModel.cats[0];
+    catListView.init();
+    catView.init();
+  },
+
+  getCurrentCat: () => {
+    return catModel.currentCat;
+  },
+
+  getCats: () => {
+    return catModel.cats;
+  },
+
+  setCurrentCat: (cat) => {
+    catModel.currentCat = cat;
+  },
+
+  incrementCatCounter: () => {
+    catModel.currentCat.counter++;
+    catView.render();
   },
 };
+
 octopus.init();
